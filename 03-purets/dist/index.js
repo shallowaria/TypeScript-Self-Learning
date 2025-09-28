24b90bd1fef1da215a6cf1c553a526b0f7a58441
shallowaria <shallowaria@gmail.com>
2025-09-28 17:44:50 +0800

    class

--- 03-purets/dist/index.js
+++ 03-purets/dist/index.js
@@ -1,13 +1,43 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Chengdu";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     email;
     name;
+    userId;
+    //不能在class外访问
+    _courseCount = 1;
     city = "Chengdu";
-    constructor(email, name) {
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
-const aria = new User("h@h.com", "aria");
+const aria = new User("h@h.com", "aria", "SI");
 export {};
 // aria.city = "SICHUAN"
+// aria.city
+// aria.deleteToken()
 //# sourceMappingURL=index.js.map
--- 03-purets/src/index.ts
+++ 03-purets/src/index.ts
@@ -1,12 +1,46 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Chengdu";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
+  //不能在class外访问
+  private _courseCount = 1;
+
   readonly city: string = "Chengdu";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: string
+  ) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  getAppleEmail(): string {
+    return `apple ${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
   }
 }
 
-const aria = new User("h@h.com", "aria");
+const aria = new User("h@h.com", "aria", "SI");
 // aria.city = "SICHUAN"
+// aria.city
+
+// aria.deleteToken()
