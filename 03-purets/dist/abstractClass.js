87d1f7d62bfb3f554a2094996cc50043ac0dead1
shallowaria <shallowaria@gmail.com>
2025-09-28 18:15:57 +0800

    abstract

---
+++ 03-purets/dist/abstractClass.js
@@ -0,0 +1,29 @@
+class TakePhoto {
+    cameraMode;
+    filter;
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        return 8;
+    }
+}
+class Instagram extends TakePhoto {
+    cameraMode;
+    filter;
+    burst;
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+const aria = new Instagram("test", "Test", 3);
+aria.getReelTime();
+export {};
+//# sourceMappingURL=abstractClass.js.map
---
+++ 03-purets/src/abstractClass.ts
@@ -0,0 +1,26 @@
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+
+  abstract getSepia(): void;
+  getReelTime(): number {
+    return 8;
+  }
+}
+
+class Instagram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter);
+  }
+
+  getSepia(): void {
+    console.log("Sepia");
+  }
+}
+
+const aria = new Instagram("test", "Test", 3);
+
+aria.getReelTime();
